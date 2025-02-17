fdae01d2844e5d2b7811195743a3a5368513ef4d
Membrizard <denis.sapegin@quantori.com>
2025-02-17 21:39:10 +0000

    working on rotations

--- frontend/stspeck/frontend/src/index.tsx
+++ frontend/stspeck/frontend/src/index.tsx
@@ -39,10 +39,11 @@ topbar.style.top = "0px"
 topbar.style.height = "30px"
 topbar.style.right = "0px"
 topbar.style.position = "absolute"
-topbar.style.background = "rgb(14, 17, 23)"
+topbar.style.background = "rgba(0, 0, 0, 0)"
 topbar.style.flexDirection = "row";
 topbar.style.alignContent = "flex-end";
 topbar.style.display = "flex";
+
 
 
 let infoc = document.createElement("div");
@@ -53,29 +54,34 @@ infoc.style.color = "#AAA";
 
 
 let autoscale = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+autoscale.innerHTML = '<g fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" transform="translate(2 2)"><path d="m16.5 5.5v-3c0-1.1045695-.8954305-2-2-2h-3"/><path d="m8.5 10.5v-4"/><path d="m6.5 8.5h4"/><path d="m16.5 11.5v3c0 1.1045695-.8954305 2-2 2h-3m-6-16h-3c-1.1045695 0-2 .8954305-2 2v3m5 11h-3c-1.1045695 0-2-.8954305-2-2v-3"/></g>'
+
 autoscale.setAttribute('width', "16");
 autoscale.setAttribute('height', "16");
-autoscale.setAttribute('viewBox', "0 0 16 16");
+autoscale.setAttribute('viewBox', "0 0 20 20");
 autoscale.setAttribute('fill', "#AAAAAA");
+autoscale.setAttribute('stroke', "#AAAAAA");
+
 autoscale.addEventListener('mouseover', function () {
-    autoscale.setAttribute('fill', "#666666");
+    autoscale.setAttribute('stroke', "#666666");
 });
 autoscale.addEventListener('mouseout', function () {
-    autoscale.setAttribute('fill', "#AAAAAA");
+    autoscale.setAttribute('stroke', "#AAAAAA");
 });
-autoscale.innerHTML = '<g><path d="M 1 5 v -4 h 4 M 15 5 v -4 h -4 M 1 11 v 4 h 4 M 11 15 h 4 v -4 M 5 8 l 3 -3 l 3 3 l -3 3 l -3 -3"></path></g>';
+// autoscale.innerHTML = '<g><path d="M 1 5 v -4 h 4 M 15 5 v -4 h -4 M 1 11 v 4 h 4 M 11 15 h 4 v -4 M 5 8 l 3 -3 l 3 3 l -3 3 l -3 -3"></path></g>';
+
 autoscale.addEventListener('click', function () {
     center();
 });
 
 let autoscalec = document.createElement("div");
-autoscalec.style.padding = "2px"
+autoscalec.style.padding = "2px 0px 0px 0px"
 autoscalec.append(autoscale)
 
 let camera = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 camera.setAttribute('width', "16");
 camera.setAttribute('height', "16");
-camera.setAttribute('viewBox', "0 0 16 16");
+camera.setAttribute('viewBox', "0 0 22 22");
 camera.setAttribute('fill', "#AAAAAA");
 camera.addEventListener('mouseover', function () {
     camera.setAttribute('fill', "#666666");
@@ -83,12 +89,14 @@ camera.addEventListener('mouseover', function () {
 camera.addEventListener('mouseout', function () {
     camera.setAttribute('fill', "#AAAAAA");
 });
-camera.innerHTML = '<g><path d="M 10.421875 8.773438 C 10.421875 10.09375 9.355469 11.160156 8.039062 11.160156 C 6.71875 11.160156 5.652344 10.09375 5.652344 8.773438 C 5.652344 7.457031 6.71875 6.390625 8.039062 6.390625 C 9.355469 6.390625 10.421875 7.457031 10.421875 8.773438 Z M 10.421875 8.773438"></path><path d="M 14.746094 4.007812 L 12.484375 4.007812 C 12.289062 4.007812 12.117188 3.929688 11.992188 3.785156 C 10.816406 2.457031 10.371094 2.015625 9.882812 2.015625 L 6.320312 2.015625 C 5.828125 2.015625 5.359375 2.460938 4.15625 3.785156 C 4.035156 3.929688 3.835938 4.007812 3.664062 4.007812 L 3.492188 4.007812 L 3.492188 3.664062 C 3.492188 3.492188 3.347656 3.320312 3.148438 3.320312 L 2.066406 3.320312 C 1.894531 3.320312 1.722656 3.464844 1.722656 3.664062 L 1.722656 4.007812 L 1.398438 4.007812 C 0.664062 4.007812 0 4.546875 0 5.285156 L 0 12.609375 C 0 13.347656 0.664062 13.984375 1.398438 13.984375 L 14.722656 13.984375 C 15.460938 13.984375 16 13.320312 16 12.609375 L 16 5.257812 C 16.023438 4.546875 15.484375 4.007812 14.746094 4.007812 Z M 8.210938 12.335938 C 6.121094 12.433594 4.398438 10.714844 4.496094 8.625 C 4.570312 6.804688 6.042969 5.332031 7.886719 5.234375 C 9.976562 5.136719 11.695312 6.855469 11.601562 8.945312 C 11.503906 10.765625 10.027344 12.242188 8.210938 12.335938 Z M 12.042969 6.414062 C 11.75 6.414062 11.503906 6.167969 11.503906 5.875 C 11.503906 5.582031 11.75 5.335938 12.042969 5.335938 C 12.335938 5.335938 12.585938 5.582031 12.585938 5.875 C 12.585938 6.167969 12.335938 6.414062 12.042969 6.414062 Z M 12.042969 6.414062 "></path></g>';
+// camera.innerHTML = '<g><path d="M 10.421875 8.773438 C 10.421875 10.09375 9.355469 11.160156 8.039062 11.160156 C 6.71875 11.160156 5.652344 10.09375 5.652344 8.773438 C 5.652344 7.457031 6.71875 6.390625 8.039062 6.390625 C 9.355469 6.390625 10.421875 7.457031 10.421875 8.773438 Z M 10.421875 8.773438"></path><path d="M 14.746094 4.007812 L 12.484375 4.007812 C 12.289062 4.007812 12.117188 3.929688 11.992188 3.785156 C 10.816406 2.457031 10.371094 2.015625 9.882812 2.015625 L 6.320312 2.015625 C 5.828125 2.015625 5.359375 2.460938 4.15625 3.785156 C 4.035156 3.929688 3.835938 4.007812 3.664062 4.007812 L 3.492188 4.007812 L 3.492188 3.664062 C 3.492188 3.492188 3.347656 3.320312 3.148438 3.320312 L 2.066406 3.320312 C 1.894531 3.320312 1.722656 3.464844 1.722656 3.664062 L 1.722656 4.007812 L 1.398438 4.007812 C 0.664062 4.007812 0 4.546875 0 5.285156 L 0 12.609375 C 0 13.347656 0.664062 13.984375 1.398438 13.984375 L 14.722656 13.984375 C 15.460938 13.984375 16 13.320312 16 12.609375 L 16 5.257812 C 16.023438 4.546875 15.484375 4.007812 14.746094 4.007812 Z M 8.210938 12.335938 C 6.121094 12.433594 4.398438 10.714844 4.496094 8.625 C 4.570312 6.804688 6.042969 5.332031 7.886719 5.234375 C 9.976562 5.136719 11.695312 6.855469 11.601562 8.945312 C 11.503906 10.765625 10.027344 12.242188 8.210938 12.335938 Z M 12.042969 6.414062 C 11.75 6.414062 11.503906 6.167969 11.503906 5.875 C 11.503906 5.582031 11.75 5.335938 12.042969 5.335938 C 12.335938 5.335938 12.585938 5.582031 12.585938 5.875 C 12.585938 6.167969 12.335938 6.414062 12.042969 6.414062 Z M 12.042969 6.414062 "></path></g>';
+camera.innerHTML = '<g><path fill-rule="evenodd" clip-rule="evenodd" d="M8 4C8 3.44772 8.41328 3 8.92308 3H15.0769C15.5867 3 16 3.44772 16 4C16 4.55228 15.5867 5 15.0769 5H8.92308C8.41328 5 8 4.55228 8 4Z"/><path fill-rule="evenodd" clip-rule="evenodd" d="M9.77778 21H14.2222C17.3433 21 18.9038 21 20.0248 20.2777C20.51 19.965 20.9267 19.5632 21.251 19.0953C22 18.0143 22 16.5095 22 13.4999C22 10.4903 21.9998 8.9857 21.2508 7.90473C20.9266 7.43676 20.5099 7.03497 20.0246 6.72228C18.9036 6 17.3431 6 14.2221 6H9.77761C6.65659 6 5.09607 6 3.97508 6.72228C3.48979 7.03497 3.07312 7.43676 2.74886 7.90473C2 8.98547 2 10.4896 2 13.4979L2 13.4999C2 16.5095 2 18.0143 2.74902 19.0953C3.07328 19.5632 3.48995 19.965 3.97524 20.2777C5.09624 21 6.65675 21 9.77778 21ZM7.83333 13.4999C7.83333 11.2808 9.69881 9.48196 12 9.48196C14.3012 9.48196 16.1667 11.2808 16.1667 13.4999C16.1667 15.7189 14.3012 17.5178 12 17.5178C9.69881 17.5178 7.83333 15.7189 7.83333 13.4999ZM9.5 13.4999C9.5 12.1685 10.6193 11.0891 12 11.0891C13.3807 11.0891 14.5 12.1685 14.5 13.4999C14.5 14.8313 13.3807 15.9106 12 15.9106C10.6193 15.9106 9.5 14.8313 9.5 13.4999ZM18.1111 9.48196C17.6509 9.48196 17.2778 9.84174 17.2778 10.2855C17.2778 10.7294 17.6509 11.0891 18.1111 11.0891H18.6667C19.1269 11.0891 19.5 10.7294 19.5 10.2855C19.5 9.84174 19.1269 9.48196 18.6667 9.48196H18.1111Z"/></g>'
+
 camera.addEventListener('click', function () {
     saveSnapshot();
 });
 let camerac = document.createElement("div");
-camerac.style.padding = "2px"
+camerac.style.padding = "2px 2px 0px 0px"
 camerac.append(camera)
 
 let palette = document.createElementNS("http://www.w3.org/2000/svg", "svg");
@@ -130,7 +138,7 @@ front.addEventListener('mouseout', function () {
 front.innerHTML = '<g><path d="M 0 5 h 10 v 10 h -10 v -10" fill="none"/><path d="M 4 0 h 10 l -4 4 h -10 l -4 4" stroke="none"/><path d="M 11 5 l 4 -4 v 10 l -4 4 v -10" stroke="none"/></g>';
 
 front.addEventListener('click', function () {
-    frontview();
+    rotate();
 });
 let frontc = document.createElement("div");
 frontc.style.padding = "2px"
@@ -278,15 +286,14 @@ container.append(canvas)
 
 // topbar.append(infoc)
 
-
-topbar.append(licc)
 topbar.append(stic)
+topbar.append(licc)
 // topbar.append(tooc)
 topbar.append(filc)
 
 topbar.append(frontc)
-topbar.append(topc)
-topbar.append(rightc)
+// topbar.append(topc)
+// topbar.append(rightc)
 
 topbar.append(palettec)
 topbar.append(camerac)
@@ -317,7 +324,7 @@ let saveSnapshot = function () {
     var imgURL = canvas.toDataURL("image/png");
     var a = document.createElement('a');
     a.href = imgURL;
-    a.download = "speck.png";
+    a.download = "molecule.png";
     document.body.appendChild(a);
     a.click();
 }
@@ -466,6 +473,28 @@ let rightview = function () {
     if (system) {
         speckView.rotateY(view, -Math.PI / 2);
         center();
+    }
+}
+
+let rotate = function () {
+  
+    let view_position = 'front'
+    if (system) {
+        if (view_position === 'front') {
+        speckView.rotateX(view, Math.PI / 2);
+        center();
+        view_position = 'top';
+        };
+        if (view_position === 'top') {
+        speckView.rotateY(view, -Math.PI / 2);
+        center();
+        view_position = 'right';
+        };
+        if (view_position === 'right') {
+        speckView.rotateX(view, Math.PI / 2);
+        center();
+        view_position = 'front';
+        }
     }
 }
 
