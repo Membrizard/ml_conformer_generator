3bf2d160e368bab76c5c57c2833c04129d4e17a7
Membrizard <denis.sapegin@quantori.com>
2025-02-21 22:51:58 +0000

    Update generate_example_samples.py

--- generate_example_samples.py
+++ generate_example_samples.py
@@ -10,12 +10,12 @@ device = "cuda"
 generator = MLConformerGenerator(device=device)
 
 example_refs = [
-                "./generation_examples/CEYYAG.mol",
-                "./generation_examples/CHEMBL223367_P10000055.mol",
-                "./generation_examples/CHEMBL3955019_P10000113.mol",
-                "./generation_examples/CHEMBL4089284_P10000009.mol",
-                "./generation_examples/CROWN6.mol",
-                "./generation_examples/YIBFEU.mol",
+                "./frontend/generation_examples/CEYYAG.mol",
+                "./frontend/generation_examples/CHEMBL223367_P10000055.mol",
+                "./frontend/generation_examples/CHEMBL3955019_P10000113.mol",
+                "./frontend/generation_examples/CHEMBL4089284_P10000009.mol",
+                "./frontend/generation_examples/CROWN6.mol",
+                "./frontend/generation_examples/YIBFEU.mol",
                 ]
 
 for i, ref in enumerate(example_refs):
@@ -32,5 +32,5 @@ for i, ref in enumerate(example_refs):
 
     results = {"aligned_reference": aligned_ref, "generated_molecules": std_samples}
 
-    with open(f"frontend/generation_examples/generation_example_{i + 1}.json", "w+") as outfile:
+    with open(f"./frontend/generation_examples/generation_example_{i + 1}.json", "w+") as outfile:
         json.dump(results, outfile)
