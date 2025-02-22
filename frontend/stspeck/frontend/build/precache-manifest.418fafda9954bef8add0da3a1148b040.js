4530a768c5e4d8b130ea74157704e3d78db6a20b
Membrizard <denis.sapegin@quantori.com>
2025-02-22 00:52:39 +0000

    built the speck

---
+++ frontend/stspeck/frontend/build/precache-manifest.418fafda9954bef8add0da3a1148b040.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68eeb23bfacda550a885cb0017a45eb1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34d61f64a8b6eede0487",
+    "url": "./static/js/2.235f16ed.chunk.js"
+  },
+  {
+    "revision": "cf9ab535e5099157adce312871dd4996",
+    "url": "./static/js/2.235f16ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa4b53aeb873155f8cc",
+    "url": "./static/js/main.660c91af.chunk.js"
+  },
+  {
+    "revision": "759b9ca7b9c36f738779",
+    "url": "./static/js/runtime-main.388eb054.js"
+  }
+]);
