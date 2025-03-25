018b7fe0b83507e7ade0163bc133c0cd86d8a15b
Membrizard <denis.sapegin@quantori.com>
2025-03-25 14:58:45 +0000

    dynamic axes disabled

--- export_edm_onnx.py
+++ export_edm_onnx.py
@@ -39,9 +39,11 @@ def prepare_dummy_input(device):
         "mad": torch.tensor([52.0409, 219.7475, 232.9718], dtype=torch.float32, device=device),
     }
 
-    n_samples = 2
+    n_samples = 100
     min_n_nodes = 18
     max_n_nodes = 20
+    fixed_max_n_nodes = 39
+
 
     # Create a random list of sizes between min_n_nodes and max_n_nodes of length n_samples
     nodesxsample = []
@@ -53,7 +55,7 @@ def prepare_dummy_input(device):
 
     batch_size = nodesxsample.size(0)
 
-    node_mask = torch.zeros(batch_size, max_n_nodes)
+    node_mask = torch.zeros(batch_size, fixed_max_n_nodes)
     for i in range(batch_size):
         node_mask[i, 0: nodesxsample[i]] = 1
 
@@ -62,7 +64,7 @@ def prepare_dummy_input(device):
     edge_mask = node_mask.unsqueeze(1) * node_mask.unsqueeze(2)
     diag_mask = ~torch.eye(edge_mask.size(1), dtype=torch.bool).unsqueeze(0)
     edge_mask *= diag_mask
-    edge_mask = edge_mask.view(batch_size * max_n_nodes * max_n_nodes, 1).to(
+    edge_mask = edge_mask.view(batch_size * fixed_max_n_nodes * fixed_max_n_nodes, 1).to(
         device
     )
     node_mask = node_mask.unsqueeze(2).to(device)
@@ -73,28 +75,28 @@ def prepare_dummy_input(device):
 
     batch_context = normed_context.unsqueeze(0).repeat(batch_size, 1)
 
-    batch_context = batch_context.unsqueeze(1).repeat(1, max_n_nodes, 1) * node_mask
-    return batch_size, max_n_nodes, node_mask, edge_mask, batch_context
+    batch_context = batch_context.unsqueeze(1).repeat(1, fixed_max_n_nodes, 1) * node_mask
+    return batch_size, fixed_max_n_nodes, node_mask, edge_mask, batch_context
 
 
 n_samples, n_nodes, node_mask, edge_mask, context = prepare_dummy_input(device)
 
 
 # onnx_model = torch.onnx.export(generative_model, dummy_input, dynamo=True)
-
+# Fixed to generate 100 samples, fixed max_n_nodes to 42 As onnx does not support dynamic axe with dynamo
 onnx_model =torch.onnx.export(
     generative_model,
     (n_samples, n_nodes, node_mask, edge_mask, context),
     "moi_edm_chembl_15_39.onnx",
     input_names=["n_samples", "n_nodes", "node_mask", "edge_mask", "context"],
     output_names=["x", "h"],
-    dynamic_axes={
-        "node_mask": {0: "batch_size", 1: "num_nodes"},
-        "edge_mask": {0: "num_edges"},
-        "context": {0: "batch_size", 1: "num_nodes"},
-        "x": {0: "batch_size", 1: "num_nodes"},
-        "h": {0: "batch_size", 1: "num_nodes"},
-    },
+    # dynamic_axes={
+    #     "node_mask": {0: "batch_size", 1: "num_nodes"},
+    #     "edge_mask": {0: "num_edges"},
+    #     "context": {0: "batch_size", 1: "num_nodes"},
+    #     "x": {0: "batch_size", 1: "num_nodes"},
+    #     "h": {0: "batch_size", 1: "num_nodes"},
+    # },
     opset_version=18,
     verbose=True,
     dynamo=True,
