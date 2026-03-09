d84c75bc56f960fc52636d6faf1e40857a74fc11
Membrizard <dasapegin@gmail.com>
2026-03-09 10:22:30 +0000

    Switched to 15-39 CONTEXT NORMS in config as main

--- src/mlconfgen/utils/config.py
+++ src/mlconfgen/utils/config.py
@@ -4,13 +4,13 @@ DIMENSION = 42
 NUM_BOND_TYPES = 5
 
 # Context norms for 15-39 model
-CONTEXT_NORMS_OLD = {
+CONTEXT_NORMS = {
     "mean": [105.0766, 473.1938, 537.4675],
     "mad": [52.0409, 219.7475, 232.9718],
 }
 
 # Context norms for 6-39 model
-CONTEXT_NORMS = {
+CONTEXT_NORMS_NEW = {
     'mean': [92.1179, 413.7875, 470.5253],
     'mad': [53.4742, 232.5775, 251.1813],
 }
